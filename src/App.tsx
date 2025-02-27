7e38983365312acf36f36f6a35b06e378040e559
OSHAN AVISHKA <153918928+oshanavishkapiries@users.noreply.github.com>
2025-02-27 14:06:19 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">ELIGHT LMS ADMIN</h1>
+      <h1>API URL: {import.meta.env.VITE_API_URL}</h1>
+      <Button variant="outline">Click me</Button>
+    </>
+  );
+}
+
+export default App;
